5a88113cc9295a921da3d6e3786d094966d00d2d
Jill Cates <cates.jill@gmail.com>
2017-06-15 13:12:08 -0400

    add a post to api routes

--- server/routes/api.js
+++ server/routes/api.js
@@ -3,7 +3,11 @@ const router = express.Router();
 
 /* GET api listing. */
 router.get('/', (req, res) => {
-    res.send('api works');
+  res.send('api works');
+});
+
+router.post('/feedback', (req, res) => {
+  res.send('POST request to the homepage');
 });
 
 module.exports = router;
