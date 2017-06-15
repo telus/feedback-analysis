2eb7fdb90166041c71a34a9a6b35586be647a254
Enakshi <eshah@nascentdigital.com>
2017-06-15 10:47:15 -0400

    added indico sentiment, emotion, and keyword api calls with custom filters

--- server.js
+++ server.js
@@ -39,15 +39,84 @@ app.set('port', port);
  */
 const server = http.createServer(app);
 
+
+
 /**
  * Listen on provided port, on all network interfaces.
  */
 server.listen(port, () => console.log(`API running on localhost:${port}`));
 
 var settings = {"api_key": "70fa9c87529dc0cd4e5dc150938f744e"};
-var response = function(res) { console.log(res); }
-var logError = function(err) { console.log(err); }
 
-indico.sentiment(['indico is so shit!', 'Still really easy, yiss'], settings)
-  .then(response)
-  .catch(logError);
+var batchInput = [
+    "This app is so shit",
+    "I can't pay my bill",
+    "I can't login since new update",
+    "Keeps crashing everytime I try to pay bills",
+    "Great app! Best out of most telecoms",
+    "keeps showing an error on offers page"
+];
+
+const appSectionsDict = [
+    "bill", "billing", "login", "usage", "data",
+    "top-up", "android", "site", "mobile", "offers",
+    "iphone", "travelpass", "travel", "plan", "plans",
+    "device", "overage", "surcharge", "overcharged",
+];
+
+const negativeKeywordsDict = [
+    "expensive", "crash", "crashes", "crashed", "crashing",
+    "pricey", "fuck", "shit", "error", "bugs",
+    "bug", "outages", "outage", "slow", "glitchy"
+]
+
+var sentimentResponse = function(res) {
+
+    for (var i = 0; i < res.length; i++) {
+
+        console.log(res[i]);
+
+        if (res[i] > 0.7) {
+            console.log("positive")
+            // write to json with positive sentiment tag
+        }
+        else if (res[i] > 0.4 && res[i] < 0.7) {
+            console.log("neutral")
+            // write to json with neutral sentiment tag
+        }
+        else {
+            console.log("negative")
+            // write to json with negative sentiment tag
+        }
+    }
+}
+
+var keywordsResponse = function (res) {
+    for (var i = 0; i < res.length; i++) {
+        let keys = Object.keys(res[i]);
+        for (var j = 0 ; j < keys.length; j++) {
+            let key = keys[j];
+            if (negativeKeywordsDict.indexOf(key.toString()) > -1) {
+                console.log("keyword found: " + key);
+                // write to json with negative keyword tag
+            }
+        }
+    }
+}
+
+var logError = function(err) {
+    console.log(err);
+}
+
+// indico.sentiment(batchInput, settings)
+//     .then(sentimentResponse)
+//     .catch(logError);
+
+// indico.emotion(batchInput, settings)
+//     .then(emotionResponse)
+//     .catch(logError);
+
+indico.keywords(batchInput, settings)
+    .then(keywordsResponse)
+    .catch(logError);
+
